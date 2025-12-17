6b372c4b57f7ec93731674d3e88025821d690f4c
TepChoulong <tepchoulongzin@gmail.com>
2025-12-17 17:13:08 +0700

    First commit

---
+++ components/NavSidebar.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarGroup,
+  SidebarHeader,
+} from "@/components/ui/sidebar";
+
+const NavSidebar = () => {
+  return <Sidebar>NavSidebar</Sidebar>;
+};
+
+export default NavSidebar;
